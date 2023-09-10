9e27f04c7566b429433ef0e9985b3bfe0e593391
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2023-09-10 10:34:06 +0530

    Update __init__.py

--- Royalkifeelings/__init__.py
+++ Royalkifeelings/__init__.py
@@ -4,36 +4,36 @@ from Royalkifeelings.callmusic.config import API_HASH, API_ID, BOT_TOKEN, OWNER_
 from Royalkifeelings.plugins import ALL_PLUGINS
 
 bot = Client(
-    "Royalkifeelings",
+    "kakashi",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
 )
 
-Royalboyamit = Client(
+kakashi = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     session_name=SESSION_NAME,
 )
 
 user = PyTgCalls(
-    Royalboyamit,
+    kakashi,
     cache_duration=100,
     overload_quiet_mode=True,
 )
 
-call_py = PyTgCalls(Royalboyamit, overload_quiet_mode=True)
+call_py = PyTgCalls(kakashi, overload_quiet_mode=True)
 
-OWNER_NAME = "Royal_boy_amit"
+OWNER_NAME = "kakashi_mina"
 F_OWNER = OWNER_ID[0]
 
-with Client("Royalkifeelings", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+with Client("kakashi", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     x = app.get_me()
     BOT_ID = x.id
     BOT_NAME = x.first_name + (x.last_name or "")
     BOT_USERNAME = x.username
     BOT_MENTION = x.mention
     BOT_DC_ID = x.dc_id
-with Royalboyamit as ass:
+with kakashi as ass:
     getass = ass.get_me()
     ASSISTANT_USERNAME = getass.username
